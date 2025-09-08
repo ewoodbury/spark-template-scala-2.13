a494367bcfdae986a13deb5288f8084877a23342
Ethan Woodbury <woodburyethan60@gmail.com>
2025-09-07 20:49:06 -0700

    Swap back name SparkTestBase

--- src/test/scala/com/example/test/SparkTestBase.scala
+++ src/test/scala/com/example/test/SparkTestBase.scala
@@ -7,7 +7,7 @@ import org.scalatest.matchers.should.Matchers
 import com.holdenkarau.spark.testing.DatasetSuiteBase
 import com.example.sparkutils.SparkPlatform
 
-trait TestSparkBase extends AnyFlatSpec with Matchers with DatasetSuiteBase with BeforeAndAfterAll with BeforeAndAfterEach {
+trait SparkTestBase extends AnyFlatSpec with Matchers with DatasetSuiteBase with BeforeAndAfterAll with BeforeAndAfterEach {
   
   override def beforeAll(): Unit = {
     super.beforeAll()
